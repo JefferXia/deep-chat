5b45b812a299813f67f7c778f43fd30774fc1914
xiazhifeng <zhifeng.xia@caocaoglobal.com>
2025-02-20 21:37:57 +0800

    r1

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,10 +1,19 @@
 import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
+// import { fireworks } from '@ai-sdk/fireworks';
+import { createDeepSeek } from "@ai-sdk/deepseek";
 import {
   customProvider,
-  extractReasoningMiddleware,
-  wrapLanguageModel,
+  // extractReasoningMiddleware,
+  // wrapLanguageModel,
 } from 'ai';
+
+export const customDeepSeek = createDeepSeek({
+  apiKey: process.env.DASHSCOPE_API_KEY,
+  baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
+  headers: {
+    Authorization: `Bearer ${process.env.DASHSCOPE_API_KEY}`,
+  },
+});
 
 export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
 
@@ -12,10 +21,11 @@ export const myProvider = customProvider({
   languageModels: {
     'chat-model-small': openai('gpt-4o-mini'),
     'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
+    "chat-model-reasoning": customDeepSeek("deepseek-r1"),
+    // 'chat-model-reasoning': wrapLanguageModel({
+    //   model: fireworks('accounts/fireworks/models/deepseek-r1'),
+    //   middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    // }),
     'title-model': openai('gpt-4-turbo'),
     'artifact-model': openai('gpt-4o-mini'),
   },
@@ -33,18 +43,18 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
+    id: 'gpt-4o-mini',
     name: 'Small model',
     description: 'Small model for fast, lightweight tasks',
   },
   {
-    id: 'chat-model-large',
+    id: 'gpt-4o',
     name: 'Large model',
     description: 'Large model for complex, multi-step tasks',
   },
   {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
+    id: 'deepseek-r1',
+    name: 'deepseek-r1满血版',
     description: 'Uses advanced reasoning',
   },
 ];
